80c845a1c7def1359520e0b6bb0f55f720243dc2
Ben Stoller <stollersystemsound@gmail.com>
2020-11-12 14:05:14 -0800

    start building out logic

--- src/js/HaikuChecker.js
+++ src/js/HaikuChecker.js
@@ -2,7 +2,42 @@ const input = (userInput).split(" ");
 input.map(e => e.split(""));
 
 vowelSearch(input) {
-input.filter(/([aeiou])\w+/gi)
-  return count = 
+transformed = input.filter(/([aeiou]e)\w+/gi)
+  return transformed
 }
 
+filter(vowelSearch())
+
+
+
+function syllableCounter (input) {
+
+  let syllableCount = 0;
+
+  let index = 0;
+  
+  function recursive (input) {
+
+    if (input.length === index) { //BASE CASE??
+      return;
+    }
+
+    if (input.length <= 3) { // weed out 3 letter words
+      syllableCount = 1;
+      return;
+    }
+
+    if (['a','e','i','o','u','y'].contains(input[index])) {
+      if (index === input.length-1) { // weed out vowels at end
+        if(input[index+1] != input[index]) { // weed out multiples
+        
+        }     
+      }
+        
+      
+    }
+  }
+  input.map(e => recursive(e))
+
+  return syllableCount
+}
