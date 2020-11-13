3b0278bbbafacc43d6f46ca34843fde55a8e31ed
Ben Stoller <stollersystemsound@gmail.com>
2020-11-12 17:31:50 -0800

    pass tests to handle full sentences
    co-author: Grant Eadie <grantleadie@gmail.com>
    Co-authored-by: AJ Markow <alexanderjmarkow@gmail.com>
    Co-Authored-By Michael Watts <wattsjmichael@gmail.com>

--- __tests__/HaikuChecker.test.js
+++ __tests__/HaikuChecker.test.js
@@ -1,31 +1,36 @@
-import  { syllableCounter, haikuCount }  from "../src/js/HaikuChecker.js";
+import  { syllableCounter, haiku }  from "../src/js/HaikuChecker.js";
 
 
   describe('Recursive approach to counting syllables', () => {
     test('recursive function', () => {
       expect(syllableCounter('cat')).toEqual(1);
       });
-    
-      test('recursive function Eagle', () => {
-        expect(syllableCounter('Eagle')).toEqual(2);
-        });
+  
+    test('recursive function Eagle', () => {
+      expect(syllableCounter('Eagle')).toEqual(2);
+      });
 
-      test('recursive function Dial', () => {
-        expect(syllableCounter('Dial')).toEqual(2);
-        });
+    test('recursive function Dial', () => {
+      expect(syllableCounter('Dial')).toEqual(2);
+      });
 
-      test('recursive function Habitat', () => {
-        expect(syllableCounter('Habitat')).toEqual(3);
-        });
+    test('recursive function Habitat', () => {
+      expect(syllableCounter('Habitat')).toEqual(3);
+      });
 
-      test('recursive function Hilarious', () => {
-        expect(syllableCounter('Hilarious')).toEqual(4);
-        
-        });
+    test('recursive function silence', () => {
+      expect(syllableCounter('Silence')).toEqual(2);
+      
+      });
 
-        test('HaikuCount', () => {
-          expect(haikuCount('An old silent pond')('A frog jumps into the pond')('splash! Silence again')).toEqual(true);
-          
-          });
+    test('HaikuCount', () => {
+      expect(haiku('An old silent pond')('A frog jumps into the pond')('splash! Silence again')).toEqual(true);
+      
+      });
+
+    test('HaikuCount', () => {
+    expect(haiku("test")("test")("test")).toEqual(false);
+      
+    });
   
 })
--- src/js/HaikuChecker.js
+++ src/js/HaikuChecker.js
@@ -1,76 +1,65 @@
-// let userInput = "cat"
-// let mapped = (userInput).split(" ");
-// let mapped = input.map(e => e.split(""));
-
-// vowelSearch(input) {
-// transformed = input.filter(/([aeiou]e)\w+/gi)
-//   return transformed
-// }
-
-// filter(vowelSearch())
-
-
 
 export function syllableCounter(userInput) {
-
+  console.log("Checking: "+userInput)
   let mapped = userInput.split("");
   let syllableCount = 0;
   let index = 0;
   let vowelArray = ['a','e','i','o','u','y']
   
   function recursive(mapped) {    
-
-    if (mapped.length === index) { //BASE CASE??
+    if (mapped.length === index) { 
       return;
     }
-
-    if (mapped.length <= 3) { // weed out 3 letter words
-      syllableCount = 1;
+    if (mapped.length <= 3) { 
+      syllableCount++;
       return;
     }
-
-    if (vowelArray.includes(mapped[index])) {
-      
-      if (mapped[index] === mapped[mapped.length-1]) { // weed out vowels at end
-        if(mapped[index] === 'e' && mapped[index-1] === 'l' && vowelArray.includes(mapped[index-2]) === false) { // weed out multiples
+    if (vowelArray.includes(mapped[index])) {      
+      if (index === mapped.length-1) {         
+        if(mapped[index] === 'e' && mapped[index-1] === 'l' && vowelArray.includes(mapped[index-2]) === false) { 
+          syllableCount++;
+        } else if (vowelArray.includes(mapped[index]) && mapped[index] != "e") {
           syllableCount++;
         }
-      } else if (vowelArray.includes(mapped[index+1]) === false) { // if a vowel is following a vowel
+      } else if (vowelArray.includes(mapped[index+1]) === false) { 
         syllableCount++;
-
       } else if (mapped[index] === 'i' && mapped[index+1] === 'a') {
         syllableCount += 1;
-
       } else if (vowelArray.includes(mapped[index+1]) && vowelArray.includes(mapped[index+2])) {
         syllableCount++;
       }
-    }
-
+    }    
     index++;
     recursive(mapped);
-  }
-  
-  recursive(mapped);
+  }  
+  recursive(mapped); 
   return syllableCount;
-}
+};
 
-export function haikuCount(firstline) {
+
+export function haiku(firstline) {
   let answer = false
-  let line1 = syllableCounter(firstline)
-  function insideOfLineHolder(secondline){
-    let line2 = syllableCounter(secondline)
-    function insideTheOtherOne(thirdline) {
-      let line3 = syllableCounter(thirdline)
-      if (line1 === 5 && line2 ===7 && line3 ===5) {
-        let answer = true;
-       return answer
+  let line1 = sentenceChopper(firstline)  
+  return function insideOfLineHolder(secondline){
+    let line2 = sentenceChopper(secondline)    
+    return function insideTheOtherOne(thirdline) {
+      let line3 = sentenceChopper(thirdline)      
+      if (line1 === 5 && line2 ===7 && line3 ===5){
+        answer = true;
+        return answer
       } 
-      else {
-        let answer = false;
-       return answer
+      else {        
+        return answer
       }
-    }
-    return answer
-  }
-  return answer
+    }    
+  }  
+};
+
+
+export function sentenceChopper(inputLine) {
+  const splitLine = (inputLine.split(" ")).map(e => syllableCounter(e));
+  const picklerMan = splitLine.reduce(function(currentValue, element) {
+    return element + currentValue;
+  }, 0);
+  return picklerMan;
 }
